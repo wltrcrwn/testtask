8626bfcd1532cc24f40612933e3f0b70f4e46b49
wltrcrwn <eleven.te.ua@gmail.com>
2020-07-30 14:42:39 +0300

    Deploy

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
 from app.csv_parser import import_customers
-from app.views import customers_list
+from app.views import customers_list, index_page
 
 urlpatterns = [
-    path('customers/', customers_list, name="customers"),
-    path('import_customers/', import_customers, name="import_customers"),
+    path('', index_page, name="index"),
+    path('api/customers/', customers_list, name="customers"),
+    path('api/import_customers/', import_customers, name="import_customers"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.shortcuts import render
 from rest_framework import generics, filters
 from app.models import Customer
 from app.serializers import CustomerSerializer
@@ -7,13 +8,16 @@ from rest_framework.response import Response
 import datetime as DT
 
 
+def index_page(request):
+    return render(request, 'app/index.html')
+
+
 @api_view(['GET'])
 def customers_list(request):
-    date = request.GET.get('date',None)
+    date = request.GET.get('date', None)
     if date:
         cs = Customer.objects.filter(register_date=DT.datetime.strptime(date, '%Y-%m-%d'))
     else:
         cs = Customer.objects.all()
     serializer = CustomerSerializer(cs, many=True)
     return Response(serializer.data)
-
